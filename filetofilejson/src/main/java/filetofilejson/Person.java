115a4451fe34e17f3ca0c2dd3542f5b2e97f70b7
KKRao <kkrao.puli@gmail.com>
2016-07-27 00:46:02 +0530

    File to File JSON

---
+++ filetofilejson/src/main/java/filetofilejson/Person.java
@@ -0,0 +1,25 @@
+package filetofilejson;
+
+public class Person {
+	
+	private int pid;
+	
+	public int getPid() {
+		return pid;
+	}
+
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	private String name;
+
+}
